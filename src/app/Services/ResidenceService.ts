3fc6b9a176ece3fa430b5597105c357ea8c745ee
hajer <hamrouni.hajer@esprit.tn>
2025-02-24 12:10:49 +0100

    MODIF SERVICE AVEC ENVIROMENT

--- src/app/Services/ResidenceService.ts
+++ src/app/Services/ResidenceService.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Residence } from '../core/models/Residence';
-
+import { environment } from 'src/environments/environment';  // Importer l'environnement
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResidenceService {
-  private residenceUrl = 'http://localhost:3000/residences';
+  private residenceUrl = environment.baseURL;  // Utiliser la base URL de l'environnement
 
   constructor(private http: HttpClient) {}
 
